29356ae277ff9866ab81f7f2ca62badc459fbe0b
Juan Navarro <navnelson7@gmail.com>
2020-08-05 18:16:44 -0600

    Agregando tareas state y su provider

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Login from './components/auth/Login';
 import NuevaCuenta from './components/auth/NuevaCuenta';
 import Proyectos from './components/proyectos/Proyectos';
+
 import ProyectoState from './context/proyectos/proyectoState';
+import TareaState from './context/tareas/tareaState';
+
 function App() {
   return (
     <ProyectoState>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-            <Route exact path="/proyectos" component={Proyectos} />
-          </Switch>
-        </Router>
+      <TareaState>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+              <Route exact path="/proyectos" component={Proyectos} />
+            </Switch>
+          </Router>
+        </TareaState>
     </ProyectoState>
   );
 }
---
+++ src/context/tareas/tareaState.js
@@ -0,0 +1,19 @@
+import React, { useReducer } from 'react';
+import TareaContext from './tareaContext';
+import TareaReducer from './tareaReducer';
+
+const TareaState = props => {
+    const initialState = {
+        tareas: [],
+    }
+
+    //crear dispatch y state
+    const [state,dispatch] = useReducer(TareaReducer,initialState);
+    return (
+        <TareaContext.Provider>
+            {props.children}
+        </TareaContext.Provider>
+    )
+}
+
+export default TareaState;
