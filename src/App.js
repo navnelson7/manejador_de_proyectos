1b4d59c848f0e78d7b68b55c299146391716b059
Juan Navarro <navnelson7@gmail.com>
2020-09-01 22:10:56 -0600

    Agregando el state de alertas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Login from './components/auth/Login';
 import NuevaCuenta from './components/auth/NuevaCuenta';
@@ -7,17 +7,21 @@ import Proyectos from './components/proyectos/Proyectos';
 import ProyectoState from './context/proyectos/proyectoState';
 import TareaState from './context/tareas/tareaState';
 
+import AlertaState from './context/alertas/alertaState';
+
 function App() {
   return (
     <ProyectoState>
       <TareaState>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Login} />
-              <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-              <Route exact path="/proyectos" component={Proyectos} />
-            </Switch>
-          </Router>
+          <AlertaState>
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+                <Route exact path="/proyectos" component={Proyectos} />
+              </Switch>
+            </Router>
+          </AlertaState>
         </TareaState>
     </ProyectoState>
   );
